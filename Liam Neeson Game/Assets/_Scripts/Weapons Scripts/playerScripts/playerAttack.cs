3a0c620760c6acae5d0fe5db4c6b3926fd65e2d2
Ishmail Qasim <IQasim94@Gmail.com>
2021-04-01 19:54:04 +0100

    Gun animation progress
    Incomplete but setting up guns

---
+++ Liam Neeson Game/Assets/_Scripts/Weapons Scripts/playerScripts/playerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour
+{
+
+    private weaponManager weapon_Manager;
+
+    public float fireRate = 15f;
+    private float nextTimeToFire;
+    public float damage = 20f;
+
+    void Awake() {
+        weapon_Manager = GetComponent<weaponManager>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        weaponShoot();
+    }
+
+    void weaponShoot() {
+        if (weapon_Manager.GetCurrentSelectedWeapon().fireType == WeaponFireType.MULTIPLE) {
+
+            if (Input.GetMouseButton(0) && Time.time > nextTimeToFire) {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                //BulletFired();
+            }
+
+        } else {
+
+            if (Input.GetMouseButtonDown(0)) {
+                weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                //BulletFired();
+            }
+
+        }
+    }
+}
---
+++ Liam Neeson Game/Assets/_Scripts/Weapons Scripts/weaponManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponManager : MonoBehaviour
+{
+    [SerializeField]
+    private weaponHandler[] weapons;
+
+    private int current_Weapon_Index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_Weapon_Index = 0;
+        weapons[current_Weapon_Index].gameObject.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            TurnOnSelectedWeapon(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { 
+            TurnOnSelectedWeapon(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { 
+            TurnOnSelectedWeapon(2);
+        }
+    }
+
+    void TurnOnSelectedWeapon(int weaponIndex)
+    {
+        if (current_Weapon_Index == weaponIndex) {
+            return;
+        }
+
+        weapons[current_Weapon_Index].gameObject.SetActive(false);
+        weapons[weaponIndex].gameObject.SetActive(true);
+        current_Weapon_Index = weaponIndex;
+    }
+
+    public weaponHandler GetCurrentSelectedWeapon() {
+        return weapons[current_Weapon_Index];
+    }
+
+}
