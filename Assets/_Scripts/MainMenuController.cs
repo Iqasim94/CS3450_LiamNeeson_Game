5d5731d264da47cf7aa197dfb3c668bfa98764f4
Ishmail Qasim <IQasim94@Gmail.com>
2021-04-01 14:18:38 +0100

    Moved to Github
    Moving existing project from PC to Github

---
+++ Assets/_Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void ButtonHandlerPlay ()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+/*	public void ButtonHandlerLoad()
+	{
+		Load feature
+	}
+*/
+	public void ButtonHandlerQuit ()
+	{
+		Application.Quit();
+	}
+
+
+}
