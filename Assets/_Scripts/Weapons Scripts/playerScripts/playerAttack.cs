75553184ff3a77530d6517ea7fd93bdbbf82c709
Ishmail Qasim <IQasim94@Gmail.com>
2021-04-07 23:17:59 +0100

    Camera Repair Issue
    Fixed the issue of weapon and armature objects disappearing as a result of a new first person camera.

--- Assets/_Scripts/Weapons Scripts/playerScripts/playerAttack.cs
+++ Assets/_Scripts/Weapons Scripts/playerScripts/playerAttack.cs
@@ -11,16 +11,16 @@ public class playerAttack : MonoBehaviour
     private float nextTimeToFire;
     public float damage = 20f;
 
-    //private Animator zoomCameraAnim;
-    //private bool zoomed;
-    //private Camera mainCam;
-    //private GameObject crosshair;
+    private Animator zoomCameraAnim;
+    private bool zoomed;
+    private Camera mainCam;
+    private GameObject crosshair;
 
     void Awake() {
         weapon_Manager = GetComponent<weaponManager>();
-        //zoomCameraAnim = transform.Find(Tags.LOOK_ROOT).transform.Find(Tags.ZOOM_CAMERA).GetComponent<Animator>();
-        //crosshair = GameObject.FindWithTag(Tags.CROSSHAIR);
-        //mainCam = Camera.main;
+        zoomCameraAnim = transform.Find(Tags.LOOK_ROOT).transform.Find(Tags.ZOOM_CAMERA).GetComponent<Animator>();
+        crosshair = GameObject.FindWithTag(Tags.CROSSHAIR);
+        mainCam = Camera.main;
     } //Awake()
 
     // Start is called before the first frame update
@@ -54,21 +54,19 @@ public class playerAttack : MonoBehaviour
         }
     } //weaponShoot()
 
-    //void ZoomInAndOut() {
-    //    if (weapon_Manager.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.AIM) {
+    void ZoomInAndOut() {
 
-    //        if (Input.GetMouseButtonDown(1)) {
-    //            zoomCameraAnim.Play(AnimationTags.ZOOM_IN_ANIM);
-    //            crosshair.SetActive(false);
-    //        }
-    //        if (Input.GetMouseButtonUp(1))
-    //        {
-    //            zoomCameraAnim.Play(AnimationTags.ZOOM_OUT_ANIM);
-    //            crosshair.SetActive(true);
-    //        }
+            if (Input.GetMouseButtonDown(1)) {
+                zoomCameraAnim.Play(AnimationTags.ZOOM_IN_ANIM);
+                crosshair.SetActive(false);
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+                zoomCameraAnim.Play(AnimationTags.ZOOM_OUT_ANIM);
+                crosshair.SetActive(true);
+            }
 
-    //    }
-    //} //ZoomInAndOut
+    } //ZoomInAndOut
 
 
 }
